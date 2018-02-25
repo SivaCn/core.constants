e046d176d31faf5e20524cfab68aba31a578b61b
Siva Cn <nsivakumar@zeomega.com>
2018-02-25 20:33:01 +0530

    added constant

--- core/constants/__init__.py
+++ core/constants/__init__.py
@@ -12,16 +12,31 @@
 
 # ----------- START: Native Imports ---------- #
 __import__('pkg_resources').declare_namespace(__name__)
+
+import logging
 # ----------- END: Native Imports ---------- #
 
 # ----------- START: Third Party Imports ---------- #
 # ----------- END: Third Party Imports ---------- #
 
 # ----------- START: In-App Imports ---------- #
-from core.constants.constants import LOG_FORMAT, central_logger_settings
 # ----------- END: In-App Imports ---------- #
 
 
 __all__ = [
     # All public symbols go here.
 ]
+
+
+AMQP_CONNECTION_STRING = 'amqp://%(username)s:%(password)s@%(host)s:%(port)s'
+
+# ------------------- START: File Logger ------------------ #
+LOG_FORMAT = '[%(asctime)s]: %(levelname)s: %(message)s'
+
+central_logger_settings = {
+    'LOG_FILE_NAME': 'central_logger.log',
+    'LOG_FILE_MAX_BYTES': 1024 * 512,
+    'LOG_FILE_BACKUP_COUNT': 5,
+    'LOG_LEVEL': logging.INFO,
+}
+# -------------------- END: File Logger ------------------- #
